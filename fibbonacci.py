b166b046c20a781239b6d621e9a9ff24f30b5b37
SamP-S <sam.ps1@outlook.com>
2024-10-07 17:44:53 +0100

    fix fibbonanci example

--- fibbonacci.py
+++ fibbonacci.py
@@ -1,22 +1,28 @@
-print("mmmmmhhhh yeaaaahhh")
 
 # functionally printing the fibbonacci sequence
 # with no depth protection
-def fibb_deep(a, b):
+def fibb_inf(a, b):
 	print (a)
-	return fibb(b, a + b)
-	#if a > 10000:
-	#	return a + b
-	#else:
-	#	return fibb(b, a + b)
-fibb_deep(1, 1)
+	return fibb_inf(b, a + b)
 
 
 # functionally printing the fibbonacci sequence
 # with max depth recursion protection defined by n
 def fibb_finite(a=1, b=1, n=10):
-	print (a)
+	print(a)
 	if n > 1:
-		return fibb(b, a + b, n-1)
+		return fibb_finite(b, a + b, n-1)
 
-fibb_finite()
+
+if __name__ == "__main__":
+	DEPTH_PROTECTION = True
+	N = 20
+	A = 1
+	B = 1
+    
+	if DEPTH_PROTECTION:
+		fibb_finite(A, B, N)
+	else:
+		fibb_inf(A, B)
+
+	print("finished")
