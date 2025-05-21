7761c1640e2d5a6fad80d9bcf6ab83562d9e140e
SamP-S <sam.ps1@outlook.com>
2025-05-21 17:39:29 +0100

    add pendulum pdf; adjust pendulum calcs

--- PyPhysics/pendulum.py
+++ PyPhysics/pendulum.py
@@ -2,42 +2,67 @@ from math import *
 import pygame as pg
 import numpy as np
 
+G = 9.81
+COLOURS = {
+    "background": pg.Color(0, 0, 0),
+    "primary": pg.Color(127, 255, 127),
+    "secondary": pg.Color(255, 127, 127),
+    "tertiary": pg.Color(127, 127, 255),
+}
+JOINT_RADIUS = 5
+LIMB_WIDTH = 2
+M_PIXEL_SCALE = 100    
+WIDTH = 800
+HEIGHT = 600
 
-def main():
+
+class Pendulum:
     
+    def __init__(self):
+        # static
+        self.l = 1
+        self.m = 10
+        self.ox = 4
+        self.oy = 4
+        
+        # dynamic
+        self.theta = pi / 2
+        self.f = self.m * G * sin(self.theta)
+        self.d_theta = 0.0
+        self.d2_theta = 0.0
+        
+    def energy(self):
+        # energy
+        kinetic = 0.5 * self.m * (self.d_theta * l) ** 2
+        potential = self.m * G * self.l * (1 - cos(self.theta))
+        total = kinetic + potential
+        print(f"ke: {kinetic};\t pe: {potential};\t total: {total}")
+
+
+def func(dt, t0, tn):
+    
+    ## calculate
+    # i = m * l**2            # moment of inertia
+    # tau = f * sin(theta) * l  # torque
+    
+    f = tn.m * G               # total linear force
+    f_perp = f * sin(theta)   # perpendicular
+    f_para = f * cos(theta)   # parallel
+    d2_theta = f_perp / m     # f = ma -> f/m = a
+    
+
+
+def main():    
     # initalise
     pg.init()
-    WIDTH = 800
-    HEIGHT = 600
     window = pg.display.set_mode((WIDTH, HEIGHT))
     pg.display.set_caption("Double Pendulum")
     clock = pg.time.Clock()
 
-    COLOURS = {
-        "background": pg.Color(0, 0, 0),
-        "primary": pg.Color(127, 255, 127),
-        "secondary": pg.Color(255, 127, 127),
-        "tertiary": pg.Color(127, 127, 255),
-    }
-    JOINT_RADIUS = 5
-    LIMB_WIDTH = 2
+    t = []
+    t0 = Pendulum()
+    t.append(t0)
     
-    # pendulum static parameters
-    l = 100
-    m = 10
-    g = 9.81
-    ox = WIDTH // 2
-    oy = 3 * HEIGHT // 4
-    
-    # pendulum dynamic parameters
-    phi = 0
-    d_phi = 2 * pi
-    d2_phi = 0
-    
-    # # centre of mass arm 1
-    # cm_x1 = 0.5 * l * sin(phi)
-    # cm_y1 = -0.5 * l * cos(phi)
-
     # main loop
     quit_flag = False
     while quit_flag == False:
@@ -50,11 +75,14 @@ def main():
                 quit_flag = True
 
         # calculate
-        phi += d_phi * dt
-        x1 = ox + l * sin(phi)
-        y1 = oy - l * cos(phi)
+        tn = Pendulum()
+        func(dt, t0, tn)
         
-        # draw
+        # graphical coordinates
+        x1 = (tn.ox + tn.l * sin(tn.theta)) * M_PIXEL_SCALE
+        y1 = (tn.oy - tn.l * cos(tn.theta)) * M_PIXEL_SCALE
+        
+        # draw commands
         window.fill(COLOURS["background"])
         pg.draw.line(window, COLOURS["secondary"], (ox, oy), (x1, y1), LIMB_WIDTH)
         pg.draw.circle(window, COLOURS["primary"], (ox, oy), JOINT_RADIUS)
@@ -62,6 +90,7 @@ def main():
 
         # swap display buffers
         pg.display.update()
+        t0 = tn
 
     # cleanup resources
     pg.quit()
