1268966f011bd1310590afa17ee7a915d137d955
SamP-S <sam.ps1@outlook.com>
2025-05-24 15:31:47 +0100

    add comparison to correct solution

--- PyPhysics/numerical.py
+++ PyPhysics/numerical.py
@@ -1,5 +1,6 @@
 from math import *
 import numpy as np
+import matplotlib.pyplot as plt 
 
 # support functions
 def pos(a):
@@ -36,31 +37,49 @@ def midpoint():
 
 if __name__ == "__main__":
     
-    
-    
     # linear equation y = m*x + c
     # y = 2x + 1
     # dy/dx = 2
+    def correct(t):
+        return 2 * t + 1
+    
     def func(t, y):
         return 2
     
     # quadratic formula y = a*x^2 + b*x + c
     # y = 0.5x^2 - 4x
     # dy/dx = x - 4
+    def correct(t):
+        return 0.5 * (t ** 2) - 4 * t
+    
     def func(t, y):
         return t - 4
 
     STEPS = 10
     t = np.zeros((STEPS))
-    y = np.zeros((STEPS))  
+    y = np.zeros((STEPS))
     
     # start
     t[0] = 0.0
     y[0] = 0.0
     for i in range(1, STEPS):
         t[i], y[i] = euler(func, t[i-1], y[i-1], 1)
-        
+
+    # calculate true values
+    c = np.apply_along_axis(correct, 0, t)        
     
     # debug
     print(f"t: {t}")
-    print(f"y: {y}")    
+    print(f"y: {y}")
+    print(f"c: {c}")
+    
+    # plot
+    plt.plot(t, y, label="Euler Approximation")
+    plt.plot(t, c, label="Correct Solution")
+    plt.xlabel("t")
+    plt.ylabel("y")
+    plt.title("Comparison of Euler Approximation and Correct Solution")
+    plt.legend()
+    plt.grid()
+    plt.show()
+    
